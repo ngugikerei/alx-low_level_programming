efce1683b7d4a9edb54a4a671ca45ef4d057795f
Michael Ngugi <117814054+ngugikerei@users.noreply.github.com>
2023-01-06 01:26:35 +0300

    my commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,17 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * _puts_recursion - print a string
- * @s:string
- * Return:void
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-}
-else
-{
-	_putchar('\n');
-}
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 
+	else
+		_putchar('\n');
 }
